9685155e79eb8468000d520eeef3f11f8929bed0
bibin-jaimon <bibinjaimon@gmail.com>
2023-10-23 23:23:40 +0530

    module setup

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -29,7 +29,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
+function Section({ children, title }: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -56,9 +56,11 @@ function Section({children, title}: SectionProps): JSX.Element {
 }
 
 function App(): JSX.Element {
-  return <View>
-    <Text>Hey there</Text>
-  </View>
+  return (
+    <View>
+      <Text>Hey there</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -80,4 +82,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export { App };
---
+++ src/screens/home/home-container.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const HomeContainer = () => {
+  return <Text>Home works</Text>;
+};
+
+export { HomeContainer };
